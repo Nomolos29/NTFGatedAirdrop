40ac70cff2af3e6bff40712682168a7d5441f248
Nomolos29 <nomolos2019@gmail.com>
2024-09-18 18:57:03 +0100

    Updates

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `${ALCHEMY_API_KEY}`, 
+      }
+    },
+
+  },
+
 };
 
 export default config;
